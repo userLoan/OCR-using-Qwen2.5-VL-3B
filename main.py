f496d6f18d0b4baedb0ce95c779466de1d683026
LoanDAM <141652400+userLoan@users.noreply.github.com>
2025-07-18 17:20:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-import os
-from openai import OpenAI
+import ollama
 
 def extract_tax_data_from_image(image_path: str) -> str:
-    token = os.environ["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"]
-    endpoint = "https://models.github.ai/inference"
-    model = "openai/gpt-4.1"
-    
-    client = OpenAI(
-        base_url=endpoint,
-        api_key=token,
-    )
-    
-    response = client.chat.completions.create(
+    response = ollama.chat(
+        model="qwen2.5vl:3b",
         messages=[
             {
-                "role": "system",
-                "content": "You are an OCR expert.",
-            },
-            {
                 "role": "user",
-                "content": "Extract the table from the image and output it as a valid JSON array.",
+                "content": ("You are an OCR expert. Extract the table from the image and output it as a valid JSON array."),
+                "images": [image_path]
             }
-        ],
-        temperature=1.0,
-        top_p=1.0,
-        model=model
+        ]
     )
-    
-    print(response.choices[0].message.content)
+    return response['message']['content'].strip()
